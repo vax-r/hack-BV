4bdeca073aefab68128c72d8664d2f269db4da72
vax-r <richard120310@gmail.com>
2023-09-30 17:42:48 +0800

    Enable connection with blend vision API * connect blend vision API with our service * write some simple API which utilize blend vision API

--- backend/code/app.py
+++ backend/code/app.py
@@ -1,31 +1,112 @@
 from flask import Flask, jsonify, request
+import http.client
+import requests
 
 app = Flask(__name__)
 
-# Sample data (you can replace this with your own data or database)
-items = [
-    {"id": 1, "name": "Item 1"},
-    {"id": 2, "name": "Item 2"},
-    {"id": 3, "name": "Item 3"},
-]
+base_url = "https://api.one-stage.kkstream.io"
+api_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjE3N2RhODVjLTllZjgtNTVjYS05M2FkLTAyYTMyZjkwZjg2MyIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJvcmJpdCIsInN1YiI6IjZiYTZhMWEzLWM2NjQtNGRmNC04N2ExLWY3NWJmZGY5MmU3NSIsImp0aSI6IjQ2Y2ViMjAyLTg4MDItNDQ4ZC05Mjg5LTc5MjEwOGRmN2U0NiIsInRva2VuX3VzZSI6ImFwaV9hY2Nlc3MiLCJlbWFpbCI6InJpY2hhcmQxMjAzMTBAZ21haWwuY29tIn0.S3Hek-V8DrdnaKWMZE19CFFmflmBBjmIcMuz8a17rb-MR0oyiRdsCyiU1lG3ueDz2PHtGT87lTIwfj7nnk2LkHypMdNXWywdf7bpcylFhVbG0dnbuPsQwpoqZcfDar-j-G5vNZookKnf-elLyHbr6g72LF-Spi6n2Zl41xeyR8LttaEYCmvQw8PTaTC2zXNX0Ev1BwGKwS699Ofup7B8ua7n38HquvRkaB8yZKJTowmSN1CeVfxh3IU1T7LInNlMdHda6X8OaEnZiZ6S3-Z4Kfbig6IVxzwgpSX7zpa79Ekl6LrYYeNlE_va7mR7QPF7pxwWB1F5TSVncp2GQSNRx-DR3GnU7ehQZZPVmmwnXqI4xdegHrj_nJV__Q9YkAF1Vep_kpEXRSb8HkF6bgwnV0rjnpLqqg5gAF2QBa5z0Rcfr-lXOqmmVz_X2Md3VR0IcZ5dp4tXyiO06x05oaE4G3HpRftMC-KGmmiNZNkNKTdtzduPzujJdipDvh5saoKc4dtGCTmR6EeWQa6XRoeBCPV-FxH36O2Gf6VFmZQDlIAPKq2jeAZuszHZwQ9EkOFDH6p_8KdEJXzm_fFVoGCfKId9_KE8NMc6qT8xKTwUEerMK4Er_lzkP1iQQcCLIRRMtKK8ppa5cHCDsIFzdqvuTloY0Xochm3JTGz1fyykqHY"
+org_id = "83f4d633-368a-4163-bbc8-359c7ff56559"
+
 
 @app.route('/welcome', methods=['GET'])
 def welcome():
     return "hello, world"
 
-# Endpoint to get a list of all items
-@app.route('/items', methods=['GET'])
-def get_items():
-    return jsonify({"items": items})
+# @app.route('/list_account', methods=['GET'])
+# def list_account():
+#     return
 
-# Endpoint to get information about a specific item
-@app.route('/items/<int:item_id>', methods=['GET'])
-def get_item(item_id):
-    item = next((item for item in items if item['id'] == item_id), None)
-    if item is not None:
-        return jsonify(item)
-    else:
-        return jsonify({"error": "Item not found"}), 404
+# list every files under your current account
+@app.route('/file_list', methods=['GET'])
+def list_files():
+    url = base_url + "/bv/cms/v1/library/files"
+    querystring = {"current_page":"1","items_per_page":"1","type":"FILE_TYPE_UNSPECIFIED"}
+    headers = {
+    "x-bv-org-id": org_id,
+    "Accept": "application/json",
+    "authorization": "Bearer " + api_token
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    return response.json()
+
+# creat VOD
+@app.route('/vod_create', methods=['POST'])
+def vod_create():
+    url = base_url + "/bv/cms/v1/vods"
+
+    payload = {
+        "metadata": {
+            "long_description": "",
+            "short_description": ""
+        },
+        "name": "IMG_1882.MOV",
+        "profile_set_id": "6a2b2ea3-9e35-4216-93c9-da09dee5ab12",
+        "pte": { "profile": "PTE_PROFILE_UNSPECIFIED" },
+        "queue": "QUEUE_STANDARD",
+        "security": {
+            "domain_control": {
+                "domains": ["https://showroom.one-dev.kkstream.io"],
+                "enabled": False
+            },
+            "geo_control": [],
+            "privacy": { "type": "SECURITY_PRIVACY_TYPE_PUBLIC" },
+            "watermark": {
+                "enabled": False,
+                "position": "WATERMARK_POSITION_BOTTOM_RIGHT",
+                "type": "WATERMARK_TYPE_IMAGE"
+            }
+        },
+        "source": {
+            "library": {
+                "subtitles": [
+                    {
+                        "code": "eng",
+                        "display": "English",
+                        "id": "ff59095c-fd67-4edc-9705-5e56c31b3577",
+                        "name": "english.vtt"
+                    }
+                ],
+                "video": { "id": "c6f50caa-37ab-4d8c-9b06-e231544d598f" }
+            },
+            "type": "SOURCE_TYPE_LIBRARY"
+        }
+    }
+    headers = {
+        "x-bv-org-id": org_id,
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "authorization": "Bearer " + api_token
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    # print(response.json())
+    return response.json()
+
+@app.route('/file_upload', methods=['POST'])
+def file_upload():
+
+    url = base_url + "/bv/cms/v1/library/files:upload"
+
+    payload = { "file": {
+            "name": request.values['name'],
+            "size": request.values['size'],
+            "source": request.values['source'],
+            "type": request.values['type']
+        } }
+    headers = {
+        "x-bv-org-id": org_id,
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "authorization": "Bearer " + api_token
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    # print(response.json())
+    return response.json()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, threaded=True, port='5000')
