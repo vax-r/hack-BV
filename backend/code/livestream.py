2f4334d11c64c5c712301fdb34128a12ab639907
hankt <hank.tu.ch@gmail.com>
2023-10-08 14:51:46 +0800

    add livestream feature to livestream.py

--- backend/code/livestream.py
+++ backend/code/livestream.py
@@ -18,23 +18,44 @@ headers = {
 # list all the livestream you have
 @livestream_bp.route('/list', methods=['GET'])
 def list():
-    # TODO : write your code below
-    return "list"
+    
+    url = base_url + "/bv/cms/v1/lives"
+    querystring = {"current_page":"1","items_per_page":"1"}
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    
+    return response.json()
 
 # create a livestream
 @livestream_bp.route('/create', methods=['GET', 'POST'])
 def create():
-    # TODO : write your code below
+    url = base_url + "/bv/cms/v1/lives/{id}:start"
+   
+
+
+
+    payload = {}
+    response = requests.post(url, json=payload, headers=headers)
+    return response.json()
     return "create"
+    
+    
 
 # edit your livestream
 @livestream_bp.route('/edit', methods=['POST'])
 def edit():
-    # TODO : write your code below
+    # TODO :
+    #  write your code below
     return "edit"
 
 # stop your livestream
 @livestream_bp.route('/stop', methods=['GET'])
 def stop():
     # TODO : write your code below
-    return "stop"
+    url = base_url + "/bv/cms/v1/library/files"
+
+    response = requests.delete(url, headers=headers)
+
+    print(response.json())
+    return response.json()
